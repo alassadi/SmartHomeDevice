a97ffdaaea67bf6194c25b878f48e5f988e133ee
felicityJane <felicity.thompson@hotmail.com>
2018-11-07 14:50:50 +0100

    Add temperature sensors outside, inside
    temperature yet to be converted to celsius inside

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -2,6 +2,9 @@ int b4 = 12;
 int b5 = 13;
 int b3 = 11;
 int b0 = 8;
+const float tempOutside = 9;
+const float tempUp = 6;
+const float tempDown = 7;
 
 String command;
 
@@ -12,69 +15,69 @@ pinMode(b4, OUTPUT);
 pinMode(b5, OUTPUT);
 pinMode(b3, OUTPUT);
 pinMode(b0, OUTPUT);
-
+pinMode(tempOutside, INPUT);
+pinMode(tempUp, INPUT);
+pinMode(tempDown, INPUT);
 
 }
-
 
 void loop() {
-  while(Serial.available()) {
+   while(Serial.available()) {
 
-command= Serial.readString();// read the incoming data as string
+       command= Serial.readString();// read the incoming data as string
 
-if(command == "1"){//serial command 1 to turn on burglar alarm
-burglarAlarmLampOn();
-Serial.println("Burglar alarm on");//serial output to pi
-} 
-else if(command == "2"){//serial input from pi: command 2 to turn off burglar alarm 
-burglarAlarmLampOff();
-Serial.println("Burglar alarm off");//serial output to pi
-} else if(command == "3"){//serial input from pi: command 3 to turn on firealarm alarm 
-turnAlarmOn();
-Serial.println("Fire alarm on");//serial output to pi
-} else if(command == "4"){//serial input from pi: command 4 to turn off firealarm alarm 
-turnAlarmOff();
-Serial.println("Fire alarm off");//serial output to pi
-}else if(command == "5"){//serial input from pi: command 5 to turn on outside lamp 
-turnOutsideLampOn();
-Serial.println("Lamp outside on");//serial output to pi
-}else if(command == "6"){//serial input from pi: command 6 to turn off outside lamp 
-turnOutsideLampOff();
-Serial.println("Lamp outside off");//serial output to pi
-} else if(command == "7"){//serial input from pi: command 7 to turn on indoor lamp 
-turnIndoorLampOn();
-Serial.println("Lamp inside on");//serial output to pi
-} else if(command == "8"){//serial input from pi: command 8 to turn off indoor lamp 
-turnIndoorLampOff();
-Serial.println("Lamp inside off");//serial output to pi
-}else if(command == "14"){//serial input from pi: command 13 to check if burglar alarm is on 
-isBurglarAlarmOn();
-}else if(command == "15"){//serial input from pi: command 14 to check if fire alarm is on 
-isAlarmOn();
-}else if(command == "16"){//serial input from pi: command 15 to check if outside lamp is on 
-isOutsideLampOn();
-}else if(command == "17"){//serial input from pi: command 16 to check if indoor lamp is on 
-isIndoorLampOn();
-}else {//serial input from pi (no such command known)
- Serial.println("Not known command"); //serial output to pi
-}
-
-}
-
+       if(command == "1"){//serial command 1 to turn on burglar alarm
+         burglarAlarmLampOn();
+         Serial.println("Burglar alarm on");//serial output to pi
+       }else if(command == "2"){//serial input from pi: command 2 to turn off burglar alarm 
+         burglarAlarmLampOff();
+         Serial.println("Burglar alarm off");//serial output to pi
+       }else if(command == "3"){//serial input from pi: command 3 to turn on firealarm alarm 
+         turnAlarmOn();
+         Serial.println("Fire alarm on");//serial output to pi
+       }else if(command == "4"){//serial input from pi: command 4 to turn off firealarm alarm 
+         turnAlarmOff();
+         Serial.println("Fire alarm off");//serial output to pi
+       }else if(command == "5"){//serial input from pi: command 5 to turn on outside lamp 
+         turnOutsideLampOn();
+         Serial.println("Lamp outside on");//serial output to pi
+       }else if(command == "6"){//serial input from pi: command 6 to turn off outside lamp 
+         turnOutsideLampOff();
+         Serial.println("Lamp outside off");//serial output to pi
+       }else if(command == "7"){//serial input from pi: command 7 to turn on indoor lamp 
+         turnIndoorLampOn();
+         Serial.println("Lamp inside on");//serial output to pi
+       }else if(command == "8"){//serial input from pi: command 8 to turn off indoor lamp 
+         turnIndoorLampOff();
+         Serial.println("Lamp inside off");//serial output to pi
+       }else if(command == "10"){
+        getTempOutside();
+       }else if(command == "11"){
+        getTempInside();
+       }else if(command == "14"){//serial input from pi: command 13 to check if burglar alarm is on 
+          isBurglarAlarmOn();
+       }else if(command == "15"){//serial input from pi: command 14 to check if fire alarm is on 
+         isAlarmOn();
+       }else if(command == "16"){//serial input from pi: command 15 to check if outside lamp is on 
+         isOutsideLampOn();
+       }else if(command == "17"){//serial input from pi: command 16 to check if indoor lamp is on 
+         isIndoorLampOn();
+       }else {//serial input from pi (no such command known)
+         Serial.println("Not known command"); //serial output to pi
+       }
+   }
 }
 void turnOutsideLampOn(){
-   digitalWrite(b4, LOW);
+  digitalWrite(b4, LOW);
   digitalWrite(b5, HIGH);
   digitalWrite(b3, HIGH);
   digitalWrite(b0, HIGH);  
-  
 }
 void turnOutsideLampOff(){
-digitalWrite(b4, HIGH);
+  digitalWrite(b4, HIGH);
   digitalWrite(b5, HIGH);
   digitalWrite(b3, HIGH);
-  digitalWrite(b0, HIGH);  
-  
+  digitalWrite(b0, HIGH);   
 }
 void isOutsideLampOn(){
   if((digitalRead(b4)==LOW)&&(digitalRead(b5)==HIGH)&&
@@ -84,23 +87,18 @@ void isOutsideLampOn(){
     Serial.println("Outside lamp is off");//serial output to pi
   }
 }
-
 void turnIndoorLampOn(){
-   digitalWrite(b4, LOW);
+  digitalWrite(b4, LOW);
   digitalWrite(b5, LOW);
   digitalWrite(b3, HIGH);
-  digitalWrite(b0, LOW);   
-  
+  digitalWrite(b0, LOW);    
 }
-
 void turnIndoorLampOff(){
-   digitalWrite(b4, HIGH);
+  digitalWrite(b4, HIGH);
   digitalWrite(b5, LOW);
   digitalWrite(b3, HIGH);
-  digitalWrite(b0, LOW); 
- 
+  digitalWrite(b0, LOW);  
 }
-
 void isIndoorLampOn(){
   if((digitalRead(b4)==LOW)&&(digitalRead(b5)==LOW)&&
   (digitalRead(b3)==HIGH)&&(digitalRead(b0)==LOW)){
@@ -109,21 +107,17 @@ void isIndoorLampOn(){
     Serial.println("Indoor lamp is off");//serial output to pi
   }
 }
-
-
 void turnAlarmOn(){
-   digitalWrite(b4, HIGH);
+  digitalWrite(b4, HIGH);
   digitalWrite(b5, LOW);
   digitalWrite(b3, LOW);
   digitalWrite(b0, LOW);  
- 
 }
 void turnAlarmOff(){
-   digitalWrite(b4, LOW);
+  digitalWrite(b4, LOW);
   digitalWrite(b5, LOW);
   digitalWrite(b3, LOW);
   digitalWrite(b0, LOW);  
-
 }
 void isAlarmOn(){
   if((digitalRead(b4)==HIGH)&&(digitalRead(b5)==LOW)&&
@@ -137,15 +131,13 @@ void burglarAlarmLampOn(){
   digitalWrite(b4, LOW);
   digitalWrite(b5, LOW);
   digitalWrite(b3, HIGH);
-  digitalWrite(b0, HIGH);
-  
+  digitalWrite(b0, HIGH); 
 }
 void burglarAlarmLampOff(){
   digitalWrite(b4, HIGH);
   digitalWrite(b5, LOW);
   digitalWrite(b3, HIGH);
   digitalWrite(b0, HIGH);
- 
 }
 void isBurglarAlarmOn(){
   if((digitalRead(b4)==LOW)&&(digitalRead(b5)==LOW)&&
@@ -154,4 +146,18 @@ void isBurglarAlarmOn(){
   }else{
     Serial.println("Burglar alarm is off");//serial output to pi
   }
+ void getTempOutside(){
+  float temp = digitalRead(temoPutside);
+  Serial.print("Temperature outside: ");//serial output to pi
+  Serial.println(temp/2);
+ }
+ //TODO: convert to celsius
+ void getTempInside(){
+  float tempUp = analogRead(tempUp);
+  float tempDown = analogRead(tempDown);
+  Serial.print("Temperature upstairs: ");//serial output to pi
+  Serial.println(tempUp);
+  Serial.print("Temperature downstairs: ");
+  Serial.println(tempDown);
+ }
 }
